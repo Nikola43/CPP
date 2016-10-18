fe2c387d5ce76d1d2f3a7ad6d95fcdeaf965e1ac
Paulo Soares <pauloxti@gmail.com>
2016-10-19 00:25:08 +0200

    subimos lo de cpp

---
+++ leerTeclas/LeerTeclas/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main( void )
+{
+   char ch;
+
+   // get the key pressed
+   ch = getchar();
+
+   cout << endl << "Key struck was " << ch;
+
+   return 0;
+}
